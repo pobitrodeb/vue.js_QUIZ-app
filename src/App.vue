bc1c9a0bcff1faacfc1a0ab3f07403d2c8fb6196
Pobitrodeb <pobitrodn@gmail.com>
2023-04-30 23:00:56 +0600

    options show done

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,110 @@
 <script  setup>
+import { ref, computed } from "vue";
 
+const questions = ref([
+  {
+    question: "What is vue.js",
+    answer: 0,
+    options: ["A Frontend Framework", "A Backend Framework", "API generator"],
+    seleted: null,
+  },
+  {
+    question: "What is Laravel",
+    answer: 1,
+    options: ["A Frontend Framework", "A Backend Framework", "API generator"],
+    seleted: null,
+  },
+  {
+    question: "What is Node.js",
+    answer: 2,
+    options: ["A Frontend Framework", "API generator", "A Backend Database"],
+    seleted: null,
+  },
+  {
+    question: "What is Node.js",
+    answer: 2,
+    options: ["A Frontend Framework", "API generator", "A Backend Database"],
+    seleted: null,
+  },
+]);
+
+const quizCompleted = ref(false);
+const currentQuestion = ref(0);
+
+const score = computed(() => {
+  let value = 0;
+  questions.value.map((q) => {
+    if (q.seleted == q.answer) {
+      value++;
+    }
+  });
+  return value;
+});
+
+const getCurrentQuestion = computed(() => {
+  let question = questions.value[currentQuestion.value];
+  question.index = currentQuestion.value;
+
+  return question;
+});
+
+const SetAnswer = (evt) => {
+  questions.value[currentQuestion.value].seleted = evt.target.value;
+  evt.target.value = null;
+};
+
+const NextQuesttion = () => {
+  if (currentQuestion.value < questions.value.length - 1) {
+    currentQuestion.value++;
+  } else {
+    quizCompleted.value = true;
+  }
+};
 </script>
 
 <template>
-   <h2 class="bg-primary text-white text-center py-3"> Quiz-App with Vue.js | www.pobitrodeb.com  </h2>
+  <h2 class="bg-primary text-white text-center py-3">
+    Quiz-App with Vue.js | www.pobitrodeb.com
+  </h2>
+
+  <div class="container">
+    <div class="card">
+      <div class="card-body">
+
+        <div class="quiz">
+          <div class="quiz-info">
+            <span class="question">{{ getCurrentQuestion.question }}</span>
+            <span class="score">
+              Score {{ score }} / {{ questions.length }}
+            </span>
+          </div>
+        </div>
+
+        <div class="options">
+         <label v-for="(option, index) in getCurrentQuestion.options" :key="index"
+         :class="`option ${
+            getCurrentQuestion.seleted == index ? index == getCurrentQuestion.answer ? 'correct' : 'wrong' : ''
+         } ${
+            getCurrentQuestion.seleted != null && index != getCurrentQuestion.seleted ? 'disable' : ''
+         }`"
+         >
+            <input 
+            type="radio" 
+            :name="getCurrentQuestion.index"
+            :value="index"
+            v-model="getCurrentQuestion.seleted"
+            :disabled="getCurrentQuestion.seleted"
+            @change="SetAnswer">
+            {{ option }}
+         </label>
+        </div>
+
+      </div>
+    </div>
+  </div>
 </template>
 
 
 
 <style scoped>
-
 </style>
